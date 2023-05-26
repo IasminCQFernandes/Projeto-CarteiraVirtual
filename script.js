3a1b34bfaeb5f2229f78dd29b5668342985cdde7
IasminFernandes <126726115+IasminCQFernandes@users.noreply.github.com>
2023-05-26 19:48:35 -0300

    finalização do add entrada de valores

--- script.js
+++ script.js
@@ -33,3 +33,77 @@
 function cadastrar(){
  alert ('CADASTRO REALIZADO COM SUCESSO! FAÇA LOGIN')
 }
+
+
+
+//ENTRADA DE VALORES
+
+//OCULTAR VALOR TOTAL
+
+var abre = false;
+
+function ocultar(){
+   
+    if(abre){
+        
+        document.getElementById("valor-total").style="display:none;";
+        abre=false;
+
+        document.getElementById('eyes').src = 'assets/no-eyes.png';
+
+        
+       
+    }else{
+    
+        document.getElementById("valor-total").style="display:block;";
+        abre = true;
+
+        document.getElementById('eyes').src = 'assets/eyes.png';
+
+       
+    }
+
+}
+
+//PEGAR ENTRADA DE VALORES
+//let addEntrada = document.getElementById('valor-entrada').value;
+
+function add(){
+  let valorTotal = document.getElementById('valor-total').value;
+  let totalNum = parseFloat(valorTotal);
+
+  
+  let addEntrada = document.getElementById('valor-entrada').value;
+  let addNum = parseFloat(addEntrada);
+
+
+  let soma = totalNum + addNum
+
+  document.getElementById('valor-total').value = soma;
+
+  obterValorSelecionado()
+
+  limpaInput();
+
+  
+
+}
+
+function limpaInput(){
+  document.getElementById('valor-entrada').value = "";
+
+}
+
+function obterValorSelecionado() {
+  var opcoes = document.getElementsByName("tipos");
+
+  for (var i = 0; i < opcoes.length; i++) {
+    if (opcoes[i].checked) {
+      var valorSelecionado = opcoes[i].value;
+      var labelSelecionado = document.querySelector("label[for='" + opcoes[i].id + "']").textContent;
+      console.log("Valor selecionado: " + valorSelecionado);
+      console.log("Label selecionado: " + labelSelecionado);
+      break; // Encerra o loop quando encontra a opção selecionada
+    }
+  }
+}
